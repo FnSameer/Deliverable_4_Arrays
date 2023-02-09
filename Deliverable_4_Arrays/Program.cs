2895106147425091abb73a51d60de46acd770390
samee <samee@LAPTOP-2MQK9SK0>
2023-02-08 22:46:33 -0500

    Add project files.

---
+++ Deliverable_4_Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Deliverable_4_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25];
+
+
+            //initialize fibonacci
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 0; i < 25; i ++)
+            {
+                if (i >= 2)
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+
+                }
+
+                Console.WriteLine("Fibonacci number (" + i + ") = " + fibonacci[i]);
+
+            }
+
+           
+        }
+    }
+}
